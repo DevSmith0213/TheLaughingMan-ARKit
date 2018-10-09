6951f1c94e7a78e3ae10e1fcd1d996da6be242d5
Jonathan Ruiz <jonjon1251@gmail.com>
2018-10-08 18:44:42 -0700

    Initial Commit

---
+++ LaughingMan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LaughingMan
+//
+//  Created by Jonathan Ruiz on 10/8/18.
+//  Copyright © 2018 Jonathan Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
