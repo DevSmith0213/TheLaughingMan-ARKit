08c2344a35a931b64ba992e489af074c41d8f8af
Jonathan Ruiz <jonjon1251@gmail.com>
2018-10-09 17:29:59 -0700

    LaughingMan project first commit

---
+++ LaughingMan/LaughingMan.swift
@@ -0,0 +1,77 @@
+//
+//  LaughingMan.swift
+//  LaughingMan
+//
+//  Created by Jonathan Ruiz on 10/8/18.
+//  Copyright © 2018 Jonathan Ruiz. All rights reserved.
+//
+
+import ARKit
+import SceneKit
+
+
+class LaughingMan: SCNNode {
+    
+    // MARK:- Properties
+    let mainNode: SCNNode
+    
+    init(geometry: ARSCNFaceGeometry) {
+        geometry.firstMaterial!.colorBufferWriteMask = [ ]
+        mainNode = SCNNode(geometry: geometry)
+        mainNode.renderingOrder = -1
+        
+        super.init()
+        self.geometry = geometry
+        
+        guard let url = Bundle.main.url(forResource: "laughingMan", withExtension: "scn", subdirectory: "Models.scnassets") else { fatalError("Missing Resource") }
+        
+        
+        let node = SCNReferenceNode(url: url)!
+        node.load()
+        
+        // configuring the node
+        guard let container = node.childNode(withName: "container", recursively: true),
+              let letterSpace = node.childNode(withName: "letterSpace", recursively: true),
+              let textCoverBorder = node.childNode(withName: "textCoverBorder", recursively: true),
+              let leftEye = node.childNode(withName: "leftEye", recursively: true),
+              let rightEye = node.childNode(withName: "rightEye", recursively: true)
+            else { return }
+        
+        
+        let rotation = SCNAction.rotateBy(x: 0, y: 0, z: 1, duration: 0.55)
+        // the phrase is 3DText objects that are childNodes to the letter space
+        // rotating the tube after the words are embeded gives the node the laughing man signature look
+        
+        container.scale = SCNVector3(0.0014, 0.0014, 0.0014) //scale of laughing man
+        letterSpace.runAction(SCNAction.repeatForever(rotation))
+        
+        if let textCoverBorder = textCoverBorder.geometry as? SCNText,
+           let leftEye = leftEye.geometry as? SCNText,
+           let rightEye = rightEye.geometry as? SCNText {
+                textCoverBorder.flatness = 0
+                leftEye.flatness = 0
+                rightEye.flatness = 0
+            
+                // to make everything look smooth you need to set the flatness for these objects in the scenekit editor to something other than 0
+                // set them to 0 in code when the node renders everything looks correct
+        }
+        
+        addChildNode(node)
+        
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("\(#function) as not been implemented")
+        
+    }
+    
+    
+    func update(withFaceAnchor anchor: ARFaceAnchor) {
+        let faceGeometry = geometry as! ARSCNFaceGeometry
+        faceGeometry.update(from: anchor.geometry)
+    }
+    
+    
+    
+}// end of LaughingMan
--- LaughingMan/ViewController.swift
+++ LaughingMan/ViewController.swift
@@ -7,14 +7,280 @@
 //
 
 import UIKit
+import ARKit
+import ReplayKit
+
+
+enum ContentType: Int {
+    case none
+    case mask
+}
+
 
 class ViewController: UIViewController {
+    // MARK:- Properties
+    let sharedRecorder = RPScreenRecorder.shared()
+    private var isRecording = false
+    var anchorNode: SCNNode?
+    var contentTypeSelected: ContentType = .none
+    var man: LaughingMan?
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    
+    // MARK:- Outlets
+    @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var maskButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sharedRecorder.delegate = self
+        sceneView.delegate = self
+        createFaceGeometry()
     }
 
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        resetTracking()
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        UIApplication.shared.isIdleTimerDisabled = false
+        sceneView.session.pause()
+    }
+    
+    
+    // MARK:- Button Actions
+    @IBAction func didTappRefreshButton(_ sender: Any) {
+        print("didTapRefresh")
+    
+        contentTypeSelected = .none
+        resetTracking()
+    }
+    
+    
+    @IBAction func didTapMaskButton(_ sender: Any) {
+        print("didTapMask")
+        
+        contentTypeSelected = .mask
+        resetTracking()
+        
+    }
+    
+    
+    @IBAction func didTapRecordButton(_ sender: Any) {
+        print("didTapRecord")
+        
+        guard sharedRecorder.isAvailable else {
+            print("Recording is not available.")
+            return
+        }
+        
+        if !isRecording {
+            startRecording()
+        } else {
+            stopRecording()
+        }
+        
+    }
+    
+    
+    
+} // end of ViewController
 
-}
 
+
+
+// MARK:- Custom Methods
+private extension ViewController {
+    
+    func resetTracking() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        configuration.providesAudioData = false
+        
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    
+    func createFaceGeometry() {
+        let device = sceneView.device!
+        let laughGeometry = ARSCNFaceGeometry(device: device)!
+        man = LaughingMan(geometry: laughGeometry)
+    }
+    
+    
+    func setupFaceNodeContent() {
+        guard let node = anchorNode else { return }
+        
+        node.childNodes.forEach { $0.removeFromParentNode() }
+        
+        switch contentTypeSelected {
+        case.none: break
+        case .mask: if let man = man { node.addChildNode(man) }
+        }
+        
+    }
+    
+    
+    
+} // end of extension
+
+
+
+// MARK:- ARSCNViewDelegate Methods Extension
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        anchorNode = node
+        setupFaceNodeContent()
+    }
+    
+   
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        
+        switch contentTypeSelected {
+        case .mask: man?.update(withFaceAnchor: faceAnchor)
+        case .none: break
+        }
+        
+    }
+    
+   
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        print("** didFailWithError")
+    }
+    
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        print("** sessionWasInterrupted")
+    }
+    
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        print("** sessionInterruptionEnded")
+    }
+    
+    
+    
+}// end of ARSCNViewDelegate extension
+
+
+
+
+// MARK:- ReplayKit Delegate Methods
+extension ViewController: RPPreviewViewControllerDelegate, RPScreenRecorderDelegate {
+    
+    func previewControllerDidFinish(_ previewController: RPPreviewViewController) {
+        print("previewControllerDidFinish")
+        dismiss(animated: true)
+    }
+    
+   
+    func screenRecorder(_ screenRecorder: RPScreenRecorder, didStopRecordingWith previewViewController: RPPreviewViewController?, error: Error?) {
+        
+        guard error == nil else {
+            print("There was an error recording: \(String(describing: error?.localizedDescription))")
+            self.isRecording = false
+            return
+        }
+    }
+    
+    
+    func screenRecorderDidChangeAvailability(_ screenRecorder: RPScreenRecorder) {
+        recordButton.isEnabled = sharedRecorder.isAvailable
+        
+        if !recordButton.isEnabled {
+            self.isRecording = false
+        }
+        
+        if sharedRecorder.isAvailable {
+            DispatchQueue.main.async {
+                self.recordButton.setTitle("🔴", for: .normal)
+            }
+        } else {
+            DispatchQueue.main.async {
+                self.recordButton.setTitle("[Can't Rec]", for: .normal)
+            }
+        }
+        
+    }
+    
+   
+    private func startRecording() {
+        self.sharedRecorder.isMicrophoneEnabled = true
+        sharedRecorder.startRecording(handler: { error in
+            guard error == nil else {
+                print("Error starting the recording: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            print("Started Recording Successfully")
+            self.isRecording = true
+            
+            DispatchQueue.main.async {
+                self.recordButton.setTitle("[STOP]", for: .normal)
+            }
+            
+        })
+    }
+    
+    
+    func stopRecording() {
+        self.sharedRecorder.isMicrophoneEnabled = false
+        sharedRecorder.stopRecording(handler: {
+            previewViewController, error in
+            guard error == nil else {
+                print("Error stopping the recording: \(String(describing: error?.localizedDescription))")
+                return
+            }
+            
+            let alert = UIAlertController(title: "Recording Complete", message: "Do you want to preview/edit your recording or delete it?", preferredStyle: .alert)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {
+                (action: UIAlertAction) in
+                self.sharedRecorder.discardRecording(handler: { () -> Void in
+                    print("Recording deleted")
+                })
+            
+            })
+            
+            let editAction = UIAlertAction(title: "Edit", style: .default, handler: { (action: UIAlertAction) -> Void in
+                if let unwrappedPreview = previewViewController {
+                    unwrappedPreview.previewControllerDelegate = self
+                    self.present(unwrappedPreview, animated: true, completion: {})
+                }
+            })
+            
+            alert.addAction(editAction)
+            alert.addAction(deleteAction)
+            self.present(alert, animated: true, completion: nil)
+        }) //end of stopRecording method
+        
+        
+        self.isRecording = false
+        DispatchQueue.main.async {
+            self.recordButton.setTitle("🔴", for: .normal)
+        }
+        
+    }
+    
+    
+    
+} // end of ReplayKit delegate methods
